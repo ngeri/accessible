4b9b86a2eee3da2af31aa7cc24ff7cc8e3df1a09
Gergely Nemeth <ngergo100@gmail.com>
2018-08-22 10:17:33 +0200

    Refactors main function.

--- Sources/AccessibleStoryboard/main.swift
+++ Sources/AccessibleStoryboard/main.swift
@@ -1,5 +1,4 @@
 import Foundation
-import IBDecodable
 import StencilSwiftKit
 import PathKit
 
@@ -15,71 +14,7 @@ guard storyboardFileNames.count > 0 else {
     exit(0)
 }
 
-var storyboardTemplates = [StoryboardTemplate]()
-configuration.storyboardFileNames.forEach({ storyboard in
-    do  {
-        log.message(.info, "Processing storyboard: \(storyboard)")
-        let file = try StoryboardFile(path: storyboard)
-
-        let viewControllerTemplates: [ViewControllerTemplate]? = file.document.scenes?.compactMap({ scene -> ViewControllerTemplate? in
-            guard let viewController = scene.viewController else  {
-                log.message(.error, "An error occured for scene with id: \(scene.id)")
-                return nil
-            }
-
-            guard let connections = viewController.viewController.connections else {
-                log.message(.info, "\(String(describing: viewController.viewController.customClass ?? "")) has no connections")
-                return nil
-            }
-            let outlets: [Outlet] = connections.compactMap {
-                guard let connection = $0.connection as? Outlet else {
-                    log.message(.info, "Connection is not an Outlet with object: \($0.connection)")
-                    return nil
-                }
-                return connection
-            }
-            var allViews = [ViewProtocol]()
-            if let rootView = viewController.viewController.rootView {
-                allViews.append(rootView)
-                allViews.append(contentsOf: rootView.getAllSubviews())
-            }
-            var viewControllerName = viewController.viewController.elementClass
-            if let customClass = viewController.viewController.customClass, let range = customClass.range(of: "ViewController") {
-                viewControllerName = String(customClass[..<range.lowerBound])
-            }
-
-            let mappedConnections = outlets.reduce(into: [ConnectionTypeTemplate.ViewType: [ConnectionTemplate]](), { result, outlet in
-                guard let view = allViews.filter({ $0.id == outlet.destination }).first else {
-                    log.message(.info, "Outlet with name '\(outlet.property)' has no view attached.")
-                    return
-                }
-
-                let viewType = ConnectionTypeTemplate.mapViewClassToViewType(type: view.elementClass)
-
-                if result[viewType] != nil {
-                    result[viewType]?.append(ConnectionTemplate(name: outlet.property))
-                } else {
-                    result[viewType] = [ConnectionTemplate(name: outlet.property)]
-                }
-            })
-
-            let templateConnections: [ConnectionTypeTemplate] = mappedConnections.map { name, connections in
-                return ConnectionTypeTemplate(name: name, connections: connections)
-            }
-
-            return ViewControllerTemplate(name: viewControllerName, connections: templateConnections)
-        })
-        guard let unwrappedViewControllerTemplates = viewControllerTemplates else {
-            log.message(.error, "IBDecodable failed to decode storyboard named: \(storyboard)")
-            return
-        }
-        let storyboardName = ((storyboard as NSString).lastPathComponent as NSString).deletingPathExtension
-        let storyboardTemplate = StoryboardTemplate(name: storyboardName, viewControllers: unwrappedViewControllerTemplates)
-        storyboardTemplates.append(storyboardTemplate)
-    } catch let error {
-        log.message(.error, "Something really bad happened: \(error)")
-    }
-})
+let storyboardTemplates = AccessibleParser.decodeStoryboards(with: storyboardFileNames)
 
 let context: [String: Any] = ["accessibiltyEnumName": configuration.enumName ?? "Accessible",
                               "date": DateFormatter.as.string(from: Date()),
